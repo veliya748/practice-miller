eced5db1fe19cc1a995cde214a180ffa5a67be82
veliya748 <veliyasavova@gmail.com>
2019-12-22 14:13:14 +0100

    Updates

---
+++ precache-manifest.e17c9e10bd02466fa097827fafc223df.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23a60e08537b9c0cc88503edfecc47a",
+    "url": "/practice-miller/index.html"
+  },
+  {
+    "revision": "1f45704db7526ba97677",
+    "url": "/practice-miller/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4f033d7f75a47e9a7869",
+    "url": "/practice-miller/static/js/2.cf6c82e0.chunk.js"
+  },
+  {
+    "revision": "a0560e08c2bea09cecbe463a56dcf52b",
+    "url": "/practice-miller/static/js/2.cf6c82e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f45704db7526ba97677",
+    "url": "/practice-miller/static/js/main.be550fd7.chunk.js"
+  },
+  {
+    "revision": "00d3d8e699a418425020",
+    "url": "/practice-miller/static/js/runtime-main.e7a0faa7.js"
+  },
+  {
+    "revision": "c840e6103f9c993f09b8257236360298",
+    "url": "/practice-miller/static/media/female assistant profile.c840e610.png"
+  },
+  {
+    "revision": "3f35d1394483ca050ff7cbf34b177359",
+    "url": "/practice-miller/static/media/healthcare.3f35d139.png"
+  },
+  {
+    "revision": "0f807958568a1a23afd33663356e317d",
+    "url": "/practice-miller/static/media/hearth.0f807958.png"
+  },
+  {
+    "revision": "692727752824ed1f880389f1bbc5fd44",
+    "url": "/practice-miller/static/media/logo.69272775.png"
+  },
+  {
+    "revision": "b34c85bdeb564a07942c3bc651886f2c",
+    "url": "/practice-miller/static/media/map.b34c85bd.PNG"
+  },
+  {
+    "revision": "9c84c66ca647ca56c7a35f09efb55aca",
+    "url": "/practice-miller/static/media/office.9c84c66c.jpg"
+  },
+  {
+    "revision": "de578f1b6cbbb6ff9ae136a6cf0000b3",
+    "url": "/practice-miller/static/media/profile doc transperent.de578f1b.png"
+  },
+  {
+    "revision": "df75fc928499f2301dff24d608c573ea",
+    "url": "/practice-miller/static/media/welcome.df75fc92.jpg"
+  }
+]);
